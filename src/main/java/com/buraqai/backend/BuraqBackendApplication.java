3314884eb0aa675cf4a0f6dfa53470068d2c052d
Noureddineblbli <nourddineblibli78@gmail.com>
2026-03-27 12:49:43 +0100

    feat: initial Spring Boot scaffold - BURAQ-68

---
+++ src/main/java/com/buraqai/backend/BuraqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buraqai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuraqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuraqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buraqai/buraq_backend/BuraqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buraqai.buraq_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuraqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
